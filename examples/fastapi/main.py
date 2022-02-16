ff81a2edcd660b8a47cef01ac14781d368cb65fc
Harsh Singh <harsh220902@gmail.com>
2022-02-16 21:12:18 +0530

    fastAPI example

---
+++ examples/fastapi/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+inventory = {
+    1: {"name": "Milk",
+       "price": 30},
+    2: {"name": "bread",
+        "price": 45}
+}
+
+@app.get("/get-item/{item_id}")
+def get_item(item_id: int):
+    return inventory[item_id]
