309532bade3e1dcf6e7cd154f6b70e4e05bb37d1
Gregor von Laszewski <laszewski@gmail.com>
2022-02-23 11:14:07 -0500

    update aws and automated scripts

--- bin/automated_yaml/anamoly_detection.py
+++ bin/automated_yaml/anamoly_detection.py
@@ -1,7 +1,17 @@
-from make_yaml import yaml_maker
 import yaml
+
+from make_yaml import yaml_maker
 
 with open("D:\\Code\\nist\\github\\nist\\catalogue_info\\oracle_ai_services\\oracle_anomaly.yaml", "w") as f:
     yaml.dump(yaml_maker(name="Oracle Anomaly Detection", title="Oracle Cloud Infrastructure", author="Oracle",
-description="Oracle Cloud Infrastructure (OCI) Anomaly Detection is an AI service that enables developers to more easily build business-specific anomaly detection models that flag critical incidents, resulting in faster time to detection and resolution. Specialized APIs and automated model selection simplify training and deploying anomaly detection models to applications and operations, all without data science expertise.",public="true", documentation="https://docs.oracle.com/en-us/iaas/Content/anomaly/using/home.htm", tags=["AI", "Anomaly Detection", "ML"], categories=["AI", "Anomaly Detection"], filename="D:\\Code\\nist\\github\\nist\\catalogue_info\\oracle_ai_services\\oracle_anomaly.yaml"),f, sort_keys=False)
-    
+                         description="Oracle Cloud Infrastructure (OCI) Anomaly Detection is an AI service that enables "
+                                     "developers to more easily build business-specific anomaly detection models that "
+                                     "flag critical incidents, resulting in faster time to detection and resolution. "
+                                     "Specialized APIs and automated model selection simplify training and deploying "
+                                     "anomaly detection models to applications and operations, all without data science "
+                                     "expertise.",
+                         public="true",
+                         documentation="https://docs.oracle.com/en-us/iaas/Content/anomaly/using/home.htm",
+                         tags=["AI", "Anomaly Detection", "ML"], categories=["AI", "Anomaly Detection"],
+                         filename="D:\\Code\\nist\\github\\nist\\catalogue_info\\oracle_ai_services\\oracle_anomaly.yaml"),
+              f, sort_keys=False)
--- bin/automated_yaml/oracle_vision.py
+++ bin/automated_yaml/oracle_vision.py
@@ -1,10 +1,17 @@
-from make_yaml import yaml_maker
 import yaml
+
+from make_yaml import yaml_maker
 
 # print(yaml.dump(yaml_maker(class_name="oracle_speech", name="Oracle Speech", title="Oracle Cloud Infrastructure", author="Oracle",
 # description="This is oracle speech", documentation="https://www.oracle.com/in/artificial-intelligence/speech/", tags=["AI", "Speech"], categories=["AI"])))
 
-with open("D:\\Code\\nist\\github\\nist\\oracle_ai_services\\oracle_vision.yaml", "w") as f:
+# Path
+
+with open("./nist/oracle_ai_services/oracle_vision.yaml", "w") as f:
     yaml.dump(yaml_maker(name="Oracle Vision", title="Oracle Cloud Infrastructure", author="Oracle",
-description="Oracle Cloud Infrastructure (OCI) Vision is an AI service that applies computer vision technology to analyze image-based content. Developers can make API calls to easily integrate pretrained models into their applications, or custom train models to meet their specific use cases. These models can be used to detect visual anomalies in manufacturing, extract text from documents to automate business workflows, and tag items in images to count products or shipments.",public="true", documentation="https://docs.oracle.com/en-us/iaas/vision/vision/using/home.htm", tags=["AI", "Vision", "Deep Learning", "Image Recognition", "Text Recognition"], categories=["AI", "Deep Learning"], filename="D:\\Code\\nist\\github\\nist\\oracle_ai_services\\oracle_vision.yaml"),f, sort_keys=False)
-    
+                         description="Oracle Cloud Infrastructure (OCI) Vision is an AI service that applies computer vision technology to analyze image-based content. Developers can make API calls to easily integrate pretrained models into their applications, or custom train models to meet their specific use cases. These models can be used to detect visual anomalies in manufacturing, extract text from documents to automate business workflows, and tag items in images to count products or shipments.",
+                         public="true", documentation="https://docs.oracle.com/en-us/iaas/vision/vision/using/home.htm",
+                         tags=["AI", "Vision", "Deep Learning", "Image Recognition", "Text Recognition"],
+                         categories=["AI", "Deep Learning"],
+                         filename="D:\\Code\\nist\\github\\nist\\oracle_ai_services\\oracle_vision.yaml"), f,
+              sort_keys=False)
